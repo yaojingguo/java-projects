efe9b7b7a7d1f3f1d2d28e304806691fc4cbc982
Jingguo Yao <yaojingguo@gmail.com>
2020-08-25 23:42:28 +0800

    update

---
+++ log4j2-code/README.md
@@ -0,0 +1,7 @@
+[Log4j 2 Compatibility with Log4j 1 API Compatibility](https://logging.apache.org/log4j/2.x/manual/compatibility.html)
+
+
+
+
+
+java -lig 'lib/log4j-api-2.13.3.jar:lib/log4j-core-2.13.3.jar' Main
---
+++ log4j2-code/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main {
+  private static Logger log = LogManager.getLogger();
+
+  public static void main(String[] args) {
+    log.info("a info message");
+  }
+}
