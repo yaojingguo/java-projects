cebc848551fc26abedbe4c8c4a48445ae1465e06
Jingguo Yao <yaojingguo@gmail.com>
2021-02-20 19:58:13 +0800

    update

--- kafka-code/src/main/java/org/yao/KafkaMain.java
+++ kafka-code/src/main/java/org/yao/KafkaMain.java
@@ -15,21 +15,22 @@ import java.util.Properties;
 import java.util.concurrent.Future;
 
 public class KafkaMain {
-  public static String topic = "quickstart-events";
+  public static String topic1 = "one";
+  public static String topic2 = "two";
 
-  private static Properties producerConfig(String portNumber) throws Exception {
+  private static Properties producerConfig(String portNumber, String clientId) throws Exception {
     Properties cfg = new Properties();
-    cfg.put("client.id", InetAddress.getLocalHost().getHostName());
+    cfg.put("client.id", clientId);
     cfg.put("bootstrap.servers", "localhost:" + portNumber);
     cfg.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     cfg.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
     return cfg;
   }
 
-  private static Properties consumerConfig(String portNumber) {
+  private static Properties consumerConfig(String portNumber, String groupId) {
     Properties props = new Properties();
     props.put("bootstrap.servers", "localhost:" + portNumber);
-    props.put("group.id", "foo");
+    props.put("group.id", groupId);
     props.put("key.deserializer", StringDeserializer.class.getName());
     props.put("value.deserializer", StringDeserializer.class.getName());
     props.put("auto.offset.reset", "earliest");
@@ -37,40 +38,51 @@ public class KafkaMain {
   }
 
   public static void main(String[] args) throws Exception {
-    KafkaProducer<String, String> kp = new KafkaProducer<String, String>(producerConfig());
+    KafkaThread t = new KafkaThread();
+    t.start();
+    sendAndPoll("9092", "clientId1", "groupId1", topic1);
+    t.join();
+  }
+
+  public static void sendAndPoll(String portNumber, String clientId, String groupId, String topic) throws Exception {
+    KafkaProducer<String, String> kp = new KafkaProducer<String, String>(producerConfig(portNumber, clientId));
     try {
       ProducerRecord<String, String> record = new ProducerRecord<>(topic, "key", "III");
-      System.out.printf("sending...\n");
+      System.out.printf(Thread.currentThread().getName()  + ":sending...\n");
       Future<RecordMetadata> future = kp.send(record);
       future.get();
-      System.out.printf("sent\n");
+      System.out.printf(Thread.currentThread().getName()  + ":sent\n");
     } finally {
       kp.close();
     }
 
-    KafkaConsumer<String, String> kc = new KafkaConsumer<String, String>(consumerConfig());
+    KafkaConsumer<String, String> kc = new KafkaConsumer<String, String>(consumerConfig(portNumber, groupId));
     kc.subscribe(Arrays.asList(topic));
 
     while (true) {
       ConsumerRecords<String, String> records = kc.poll(Duration.ofSeconds(5));
       int count = records.count();
-      System.out.printf("polled %d records\n", count);
+      System.out.printf(Thread.currentThread().getName()  + ": polled %d records\n", count);
       if (count == 0) {
-        System.out.printf("exit the loop");
+        System.out.printf(Thread.currentThread().getName()  + ":exit the loop");
         break;
       }
 
       for (ConsumerRecord<String, String> rec : records) {
-        System.out.printf("key: %s, key: %s\n", rec.key(), rec.value());
+        System.out.printf(Thread.currentThread().getName()  + ":key: %s, key: %s\n", rec.key(), rec.value());
       }
     }
     kc.close();
   }
 
-  class KafkaThread extends Thread {
+  static class KafkaThread extends Thread {
     @Override
     public void run() {
-
+      try {
+        sendAndPoll("9093", "clientId2", "groupId2", topic2);
+      } catch (Throwable t) {
+        throw new RuntimeException(t);
+      }
     }
   }
 
