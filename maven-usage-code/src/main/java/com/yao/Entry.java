54fcbc7f097e2f858d3f9594d0e420fba72b3b55
Jingguo Yao <yaojingguo@gmail.com>
2021-03-14 16:19:48 +0800

    update

--- maven-usage-code/src/main/java/com/yao/Entry.java
+++ maven-usage-code/src/main/java/com/yao/Entry.java
@@ -5,5 +5,6 @@ import org.yao.HelloWorld;
 public class Entry {
   public static void main(String[] args) {
     HelloWorld.greet();  
+    System.out.println("I am leaving");
   }
 }
---
+++ maven-usage-code/src/test/java/org/yao/EntryTest.java
@@ -0,0 +1,12 @@
+package org.yao;
+
+import org.junit.Test;
+import static com.google.common.truth.Truth.assertThat;
+
+public class EntryTest {
+  @Test
+  public void test() {
+    HelloWorld.greet();
+  }
+  
+}
