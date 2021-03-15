f63c2af0b8439019bbd82da4889c80aa741ae540
Jingguo Yao <yaojingguo@gmail.com>
2021-03-15 14:50:50 +0800

    update

--- sky-log/src/main/java/org/yao/HelloController.java
+++ sky-log/src/main/java/org/yao/HelloController.java
@@ -1,13 +1,26 @@
 package org.yao;
 
+import java.time.LocalDate;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private Logger logger = LogManager.getLogger(getClass());
+
     @GetMapping("/greeting")
     public String greeting() {
+        logger.info("greeting people");
         return "hello, world";
+    }
+
+    @GetMapping("/time")
+    public String time() {
+        logger.info("current time");
+        LocalDate date = LocalDate.now();
+        return date.toString();
     }
 }
 
