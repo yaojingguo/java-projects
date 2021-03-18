eb3bdc582bf895086307d6baf3ab863d828b94cd
Jingguo Yao <yaojingguo@gmail.com>
2021-03-18 19:30:59 +0800

    update

--- sky-log/src/main/java/org/yao/HelloController.java
+++ sky-log/src/main/java/org/yao/HelloController.java
@@ -1,14 +1,20 @@
 package org.yao;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     private Logger logger = LogManager.getLogger(getClass());
+
+    private List<String> names = new ArrayList<>();
 
     @GetMapping("/greeting")
     public String greeting() {
@@ -21,6 +27,17 @@ public class HelloController {
         logger.info("current time");
         LocalDate date = LocalDate.now();
         return date.toString();
+    }
+
+    @PostMapping("/name")
+    public String addPerson(@RequestBody String name) {
+        names.add(name);
+        return name;
+    }
+
+    @GetMapping("/name")
+    public String person() {
+        return names.toString();
     }
 }
 
