d6da6535255e26a80480e5a795e40df7c537e599
Jingguo Yao <yaojingguo@gmail.com>
2021-04-01 17:39:12 +0800

    update

--- sky-log/src/main/java/org/yao/ToolkitController.java
+++ sky-log/src/main/java/org/yao/ToolkitController.java
@@ -26,50 +26,50 @@ public class ToolkitController {
     System.out.printf("done\n");
   }
 
-    @GetMapping("trace-user")
-    public User traceUser() {
-      return avatar(10, "Jack");
-    }
+  @GetMapping("trace-user")
+  public User traceUser() {
+    return avatar(10, "Jack");
+  }
 
-    @GetMapping("tag")
-    public User onlyTag() {
-      return incrementAge(10);
-    }
+  @GetMapping("tag")
+  public User onlyTag() {
+    return incrementAge(10);
+  }
 
-    @GetMapping("tag-indirect")
-    public User tagIndirect() {
-      return bridge();
-    }
+  @GetMapping("tag-indirect")
+  public User tagIndirect() {
+    return bridge();
+  }
 
-    @Trace
-    private User bridge() {
-      return incrementAge(100);
-    }
+  @Trace
+  private User bridge() {
+    return incrementAge(100);
+  }
 
-    @GetMapping("tags")
-    public User onlyTags() {
-      return zero(10, "Jack");
-    }
+  @GetMapping("tags")
+  public User onlyTags() {
+    return zero(10, "Jack");
+  }
 
-    @Trace
-    @Tag(key = "age", value = "arg[0]")
-    @Tag(key = "name", value = "arg[1]")
-    @Tag(key = "name", value = "returnedObj.name")
-    @Tag(key = "age", value = "returnedObj.age")
-    public User avatar(int originalAge, String originalName) {
-      User u = new User(originalAge + 1, originalName + "-pandora");
-      return u;
-    }
+  @Trace
+  @Tag(key = "age", value = "arg[0]")
+  @Tag(key = "name", value = "arg[1]")
+  @Tag(key = "name", value = "returnedObj.name")
+  @Tag(key = "age", value = "returnedObj.age")
+  public User avatar(int originalAge, String originalName) {
+    User u = new User(originalAge + 1, originalName + "-pandora");
+    return u;
+  }
 
-    @Tag(key = "age", value = "arg[0]")
+  @Tag(key = "age", value = "arg[0]")
   //  @Tag(key = "op", value = "only-tag")
-    public User incrementAge(int age) {
-      System.out.printf(
-          "trace id: %s, segment id: %s, span id: %s\n", TraceContext.traceId(),
-   TraceContext.segmentId(), TraceContext.spanId());
-      User u = new User(age - 1, "xiaoyu");
-      return u;
-    }
+  public User incrementAge(int age) {
+    System.out.printf(
+        "trace id: %s, segment id: %s, span id: %s\n",
+        TraceContext.traceId(), TraceContext.segmentId(), TraceContext.spanId());
+    User u = new User(age - 1, "xiaoyu");
+    return u;
+  }
 
   @Tags({@Tag(key = "age", value = "arg[0]"), @Tag(key = "name", value = "returnedObj.name")})
   public User zero(int age, String name) {
@@ -77,26 +77,26 @@ public class ToolkitController {
     return u;
   }
 
-    @GetMapping("active")
-    public String active() {
-      ActiveSpan.setOperationName("try-use-toolkit");
-      ActiveSpan.tag("homeland", "China");
-      ActiveSpan.info("set up a classroom");
-      return "active response";
-    }
+  @GetMapping("active")
+  public String active() {
+    ActiveSpan.setOperationName("try-use-toolkit");
+    ActiveSpan.tag("homeland", "China");
+    ActiveSpan.info("set up a classroom");
+    return "active response";
+  }
 
-    @GetMapping("active-error-occurred")
-    public String activeErrorOccurred() {
-      ActiveSpan.setOperationName("operation-with-error-occurred");
-      ActiveSpan.tag("city", "Beijing");
-      ActiveSpan.error("failed to add a student to the classroom");
-      return "active-error-occurred response";
-    }
+  @GetMapping("active-error-occurred")
+  public String activeErrorOccurred() {
+    ActiveSpan.setOperationName("operation-with-error-occurred");
+    ActiveSpan.tag("city", "Beijing");
+    ActiveSpan.error("failed to add a student to the classroom");
+    return "active-error-occurred response";
+  }
 
-    @GetMapping("active-throwable")
-    public String activeThrowable() {
-      ActiveSpan.setOperationName("operation-with-throwable");
-      ActiveSpan.error(new Throwable());
-      return "active-throwable response";
-    }
+  @GetMapping("active-throwable")
+  public String activeThrowable() {
+    ActiveSpan.setOperationName("operation-with-throwable");
+    ActiveSpan.error(new Throwable());
+    return "active-throwable response";
+  }
 }
--- spring-boot-code/src/main/java/org/yao/RedisCode.java
+++ spring-boot-code/src/main/java/org/yao/RedisCode.java
@@ -1,17 +1,28 @@
 package org.yao;
 
-import io.lettuce.core.*;
+import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
 
 
 public class RedisCode {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Throwable {
+    int seconds = 60;
+    System.out.printf("sleeping for %d seconds...\n", seconds);
+    Thread.sleep(seconds* 1000);
+    accessRedis();
+  }
+
+  private static void accessRedis() throws Throwable {
     RedisClient redisClient = RedisClient.create("redis://password@localhost:6379/0");
     StatefulRedisConnection<String, String> connection = redisClient.connect();
     RedisCommands<String, String> syncCommands = connection.sync();
 
-    syncCommands.set("key", "Hello, Redis!");
+    String value = syncCommands.get("mykey");
+
+    System.out.println();
+    System.out.printf("value: %s\n", value);
+    System.out.println();
 
     connection.close();
     redisClient.shutdown();
