f722cdb9b516a3d8cbb8b7723b18f5b5d263f351
Jingguo Yao <yaojingguo@gmail.com>
2021-04-05 19:19:15 +0800

    update

---
+++ lucene-code/src/main/java/com/yao/Entry.java
@@ -0,0 +1,10 @@
+package com.yao;
+
+import org.yao.HelloWorld;
+
+public class Entry {
+  public static void main(String[] args) {
+    HelloWorld.greet();  
+    System.out.println("I am leaving");
+  }
+}
