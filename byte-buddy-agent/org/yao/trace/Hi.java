62c88573997047a7d3608953e87728e5c6b420d0
Jingguo Yao <yaojingguo@gmail.com>
2021-04-25 20:18:38 +0800

    update

--- byte-buddy-agent/org/yao/trace/Hi.java
+++ byte-buddy-agent/org/yao/trace/Hi.java
@@ -1,10 +1,13 @@
 package org.yao.trace;
 
+
+@Deprecated
 public class Hi {
   public static void main(String[] args) throws InterruptedException {
     Hi h = new Hi();
     h.hi();
     h.hiWithName("xiaoyu");
+    System.out.printf("toString: %s\n", h);
   }
 
   private void hi() throws InterruptedException {
--- byte-buddy-agent/src/main/java/org/yao/SimpleAgent.java
+++ byte-buddy-agent/src/main/java/org/yao/SimpleAgent.java
@@ -6,7 +6,6 @@ import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.FixedValue;
 import net.bytebuddy.utility.JavaModule;
 
-import java.lang.annotation.Documented;
 import java.lang.instrument.Instrumentation;
 
 import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
@@ -16,7 +15,7 @@ public class SimpleAgent {
 
   public static void premain(String arguments, Instrumentation instrumentation) {
     new AgentBuilder.Default()
-        .type(isAnnotatedWith(Documented.class))
+        .type(isAnnotatedWith(Deprecated.class))
         .transform(
             new AgentBuilder.Transformer() {
               @Override
@@ -25,8 +24,8 @@ public class SimpleAgent {
                   TypeDescription typeDescription,
                   ClassLoader classLoader,
                   JavaModule module) {
-                System.out.printf("instrumented");
-                return builder.method(named("toString")).intercept(FixedValue.value("transformed"));
+                System.out.printf("[Agent] instrumenting\n");
+                return builder.method(named("toString")).intercept(FixedValue.value("fixed toString value"));
               }
             })
         .installOn(instrumentation);
