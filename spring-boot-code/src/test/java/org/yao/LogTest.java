d49325577aa7535a796d85d096eaf6333c322bee
Jingguo Yao <yaojingguo@gmail.com>
2021-04-27 11:36:46 +0800

    update

---
+++ spring-boot-code/src/test/java/org/yao/LogTest.java
@@ -0,0 +1,15 @@
+package org.yao;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+
+public class LogTest {
+  private Logger log = LogManager.getLogger();
+
+  @Test
+  public void testLoggging() {
+    log.info("an info message");
+  }
+}
