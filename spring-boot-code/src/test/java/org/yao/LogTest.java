dab7f49d9613251f5e29ca2cbb586f579ddb475b
Jingguo Yao <yaojingguo@gmail.com>
2021-04-27 16:53:58 +0800

    update

--- spring-boot-code/src/test/java/org/yao/LogTest.java
+++ spring-boot-code/src/test/java/org/yao/LogTest.java
@@ -4,11 +4,19 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
+import static org.yao.MyMessage.m;
+
 public class LogTest {
   private Logger log = LogManager.getLogger();
 
   @Test
   public void testLoggging() {
     log.info("an info message");
+  }
+
+  @Test
+  public void testStructured() {
+    log.error(m("entered classroom", "cid", "10", "uid", "10"), new Throwable("abc"));
+    log.error(m("entered classroom", new Throwable("def"), "cid", "10", "uid", "10"));
   }
 }
