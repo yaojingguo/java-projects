04c975a6e3e23cad877e67f2de263625a1560225
Jingguo Yao <yaojingguo@gmail.com>
2021-07-21 19:35:22 +0800

    update

---
+++ prometheus/src/main/java/org/yao/Main.java
@@ -0,0 +1,30 @@
+package org.yao;
+
+import io.prometheus.client.Counter;
+import io.prometheus.client.exporter.HTTPServer;
+import java.io.IOException;
+
+public class Main {
+  private static Counter oneCounter =
+      Counter.build().name("one_counter").help("No.1 counter").register();
+
+  public static void main(String[] args) throws IOException {
+    HTTPServer server = new HTTPServer(1234);
+
+    new Thread(() -> {
+      for (;;) {
+        oneCounter.inc();
+        nap(2);
+      }
+    }).start();
+  }
+
+  private static void nap(long seconds) {
+    try {
+      System.out.printf("napping %d seconds\n", seconds);
+      Thread.sleep(seconds * 1000);
+    } catch (InterruptedException ex) {
+      throw new RuntimeException(ex);
+    }
+  }
+}
