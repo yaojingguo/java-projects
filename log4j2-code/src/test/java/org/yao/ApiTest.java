05b0746bee9499c920fab5550fe76d371c088bde
Jingguo Yao <yaojingguo@gmail.com>
2021-08-30 22:48:14 +0800

    update

---
+++ log4j2-code/src/test/java/org/yao/ApiTest.java
@@ -0,0 +1,15 @@
+package org.yao;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ApiTest {
+
+  private Logger log = LoggerFactory.getLogger(getClass());
+
+  @Test
+  public void testUsage() {
+    log.info("an info message");
+  }
+}
