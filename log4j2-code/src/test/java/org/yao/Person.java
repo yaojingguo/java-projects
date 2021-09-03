20aa516021029aa67cf017f30a2acc279c72ed7d
Jingguo Yao <yaojingguo@gmail.com>
2021-09-03 22:01:03 +0800

    update

---
+++ log4j2-code/src/test/java/org/yao/Person.java
@@ -0,0 +1,19 @@
+package org.yao;
+
+class Person {
+  private int no;
+  private String name;
+
+  public Person(int no, String name) {
+    this.no = no;
+    this.name = name;
+  }
+
+  public int getNo() {
+    return no;
+  }
+
+  public void setNo(int no) {
+    this.no = no;
+  }
+}
