e7010138dcef15e469f5c4aff58396da76052570
Jingguo Yao <yaojingguo@gmail.com>
2021-09-03 14:49:22 +0800

    update

--- log4j2-code/src/test/java/org/yao/LogTest.java
+++ log4j2-code/src/test/java/org/yao/LogTest.java
@@ -4,8 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
