f66a8019457d977836389e2809c95f08b7547932
Jingguo Yao <yaojingguo@gmail.com>
2021-11-08 16:44:16 +0800

    update

--- gs-actuator-service/src/main/java/com/example/actuatorservice/HelloWorldController.java
+++ gs-actuator-service/src/main/java/com/example/actuatorservice/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.example.actuatorservice;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +16,25 @@ public class HelloWorldController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
+
+	@Autowired
+	private JdbcTemplateDao dao;
+
 	@GetMapping("/hello-world")
 	@ResponseBody
 	public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
+	@GetMapping("/classes")
+	@ResponseBody
+	public List classes() {
+		return dao.classes();
+	}
 
+	@GetMapping("/start")
+	@ResponseBody
+	public String start() {
+		return dao.stress();
+	}
 }
