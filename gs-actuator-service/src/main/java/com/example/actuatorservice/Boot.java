d9f301fc115fd9c0132d77aaf6fe779f5221dbb5
Jingguo Yao <yaojingguo@gmail.com>
2021-11-08 18:12:41 +0800

    update

---
+++ gs-actuator-service/src/main/java/com/example/actuatorservice/Boot.java
@@ -0,0 +1,24 @@
+package com.example.actuatorservice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(Greeting.class)
+public class Boot {
+
+  private Logger log = LogManager.getLogger();
+
+  @Bean(initMethod = "init")
+  @ConditionalOnMissingBean
+  public Greeting greet() {
+    Greeting o = new Greeting(1, "Nihao");
+    log.info("created a greeting");
+    return o;
+  }
+
+}
