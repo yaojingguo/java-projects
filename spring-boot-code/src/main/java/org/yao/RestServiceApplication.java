a6d724c941dadda36316239a02c680d385be6260
Jingguo Yao <yaojingguo@gmail.com>
2021-11-16 14:49:26 +0800

    update

--- spring-boot-code/src/main/java/org/yao/RestServiceApplication.java
+++ spring-boot-code/src/main/java/org/yao/RestServiceApplication.java
@@ -3,14 +3,9 @@ package org.yao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Make spring scan components in com.yao and org.yao base p
-// ackages.
-// @ComponentScan(basePackages = {"com.yao", "org.yao"})
-@SpringBootApplication
-//@EnableMetrics
-//@EnablePrometheusMetrics
-public class RestServiceApplication {
 
+@SpringBootApplication
+public class RestServiceApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestServiceApplication.class, args);
   }
