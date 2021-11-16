c64bd1738c13dea01305b5f1e3aa9bc0cc0a9d62
Jingguo Yao <yaojingguo@gmail.com>
2021-11-16 15:44:08 +0800

    update

--- spring-boot-test/src/test/java/com/example/testingweb/SmokeTest.java
+++ spring-boot-test/src/test/java/com/example/testingweb/SmokeTest.java
@@ -9,19 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SmokeTest {
-
-	@Autowired
-	private HomeController controller;
-
 	@Autowired
 	private MyBean myBean;
 
 	@Test
 	public void contextLoads() throws Exception {
-		assertThat(controller).isNotNull();
 		assertThat(myBean).isNotNull();
-
-		System.out.printf("controller: %s\n", controller);
 		System.out.printf("myBean: %s\n", myBean);
 	}
 }
