b3b4bcfe99a0f7039cc456f76b2273f4165ded25
Jingguo Yao <yaojingguo@gmail.com>
2021-12-13 15:46:01 +0800

    update

---
+++ gs-testing-web/src/main/java/Entry.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+/**
+ * {@link java.util.Map}
+ */
+public class Entry {
+  public static void main(String[] args) {
+    System.out.println("hello, world");
+    System.out.printf("hello, world\n");
+  }
+}
