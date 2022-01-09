abb7472b37505b0766487d8a59e698c0f19b3f4f
Jingguo Yao <yaojingguo@gmail.com>
2022-01-09 10:12:16 +0800

    update

--- micrometer-code/src/main/java/org/yao/Main.java
+++ micrometer-code/src/main/java/org/yao/Main.java
@@ -1,6 +1,7 @@
 package org.yao;
 
 import com.sun.net.httpserver.HttpServer;
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 
@@ -10,20 +11,41 @@ import java.net.InetSocketAddress;
 
 public class Main {
   public static void main(String[] args) {
-    PrometheusMeterRegistry prometheusRegistry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+    PrometheusMeterRegistry prometheusRegistry =
+        new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
 
     try {
       HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-      server.createContext("/prometheus", httpExchange -> {
-        String response = prometheusRegistry.scrape();
-        httpExchange.sendResponseHeaders(200, response.getBytes().length);
-        try (OutputStream os = httpExchange.getResponseBody()) {
-          os.write(response.getBytes());
-        }
-      });
+
+      final Counter counter = prometheusRegistry.counter("xiao_counter", "age", "12");
+      new Thread(
+              () -> {
+                System.out.printf("incrementing...\n");
+                counter.increment();
+                System.out.printf("incremented");
+              })
+          .start();
+
+      server.createContext(
+          "/prometheus",
+          httpExchange -> {
+            String response = prometheusRegistry.scrape();
+            httpExchange.sendResponseHeaders(200, response.getBytes().length);
+            try (OutputStream os = httpExchange.getResponseBody()) {
+              os.write(response.getBytes());
+            }
+          });
 
       new Thread(server::start).start();
     } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void nap(long seconds) {
+    try {
+      Thread.sleep(1000 * seconds);
+    } catch (InterruptedException e) {
       throw new RuntimeException(e);
     }
   }
