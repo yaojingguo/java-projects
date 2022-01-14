3afa9b156f2455eb12792b7ddaadea81dcc2a1b8
Jingguo Yao <yaojingguo@gmail.com>
2022-01-14 14:58:22 +0800

    update

--- kafka-code/src/main/java/org/yao/Main.java
+++ kafka-code/src/main/java/org/yao/Main.java
@@ -3,10 +3,13 @@ package org.yao;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.Metric;
+import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Properties;
 
 public class Main {
@@ -24,21 +27,48 @@ public class Main {
     try (KafkaConsumer<String, String> consumer = createConsumer(); ) {
       consumer.subscribe(Arrays.asList("apiTopic"));
       boolean done = false;
-      while (true) {
-        long seconds = 2;
-        System.out.printf("polling for %d seconds\n", seconds);
+
+      for (int i = 0; i < 100; i++) {
+        long seconds = 1;
+        System.out.printf("%d: polling for %d seconds\n", i, seconds);
         ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(seconds));
-        System.out.printf("polled\n");
+        System.out.printf("polled %d records\n", records.count());
         if (records.count() > 0) {
           for (ConsumerRecord<String, String> record : records) {
             System.out.println(record.offset() + ": " + record.value());
           }
         } else {
-          System.out.println("got nothing");
+          System.out.printf("got nothing\n");
         }
+        report(consumer);
+      }
+
+      for (int i = 0; i < 5; i++) {
+        nap(3);
+        report(consumer);
       }
     }
   }
+
+  private static void report(KafkaConsumer<String, String> consumer) {
+    Map<MetricName, ? extends Metric> map = consumer.metrics();
+    for (Metric v: map.values()) {
+      String name = "last-poll-seconds-ago";
+      if (v.metricName().name().equals(name)) {
+        System.out.printf("%s: %s\n", name, v.metricValue());
+      }
+    }
+  }
+
+  private static void nap(long seconds) {
+    try {
+      System.out.printf("sleeping for %d seconds...\n", seconds);
+      Thread.sleep(seconds * 1000);
+    } catch (InterruptedException e) {
+      throw  new RuntimeException(e);
+    }
+  }
+
 
   public static void main(String[] args) {
     verifyAutoCommit();
