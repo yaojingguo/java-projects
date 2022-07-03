d3cdd8a2c27c1ed6d8b922717df9e7dd4c9aad37
Jingguo Yao <yaojingguo@gmail.com>
2022-07-03 21:24:30 +0800

    update

---
+++ pdf/src/main/java/org/yao/Util.java
@@ -0,0 +1,10 @@
+package org.yao;
+
+public class Util {
+  public static final String OUT_DIR = "out";
+
+  public static String outPathname(String filename) {
+    return OUT_DIR + "/" + filename;
+  }
+
+}
